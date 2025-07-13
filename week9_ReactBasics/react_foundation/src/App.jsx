f6de129e34ded30cd03e7e11ec3ac73822b3c80e
aaryanwadhawan7 <aaryanwadhawam339@gmail.com>
2025-07-13 23:20:12 +0530

    State, Components, useEffect, useState, dependency arr, cleanup logic covered

--- week9_ReactBasics/react_foundation/src/App.jsx
+++ week9_ReactBasics/react_foundation/src/App.jsx
@@ -1,95 +1,80 @@
+import { PostComponent } from "./Post";
+import { Notification } from "./Notification";
 import { useState } from "react";
 
 function App() {
-  return (
-  <div style={{display : "flex", justifyContent : "center", flexDirection : "column"}}>
-  <div style={{backgroundColor : "#4682B4", margin : 20, width : 400, padding : 10, borderRadius : 15}}>
-  <PostComponent
-  username = {"Aaryan Wadhawan"}
-  followerCount = {"20,000 followers"}
-  time = {"12 min"}
-  ></PostComponent>
-  <Notification></Notification>
-  </div>
-  <br></br>
-  <div style={{backgroundColor : "#4682B4", margin : 20, width : 400, padding : 10, borderRadius : 15}}>
-  <PostComponent
-  username = {"Kiwi Wadhawan"}
-  followerCount = {"Promoted"}
-  ></PostComponent>
-  <Notification></Notification>
-  </div>
-  </div>
-  )
-}
+  const [posts, setPosts] = useState([]);
 
-// Structuring your app into components
-// Defining a state of your application
-
-const Notification = () => {
-  // Defining a state variable
-  // Component will only re-reder when there is a state variable
-
-  const [notificationCount, setNotificationCount] = useState (0);
-  // [0, function ()]
-  
-  function increment () {
-    setNotificationCount(notificationCount+1);
+  function addPost() {
+    const createPost = {
+      id: posts.length + 1,
+      username: "Aaryan Wadhawan",
+      followerCount: "20,000 followers",
+      time: "15 min",
+    };
+    setPosts([...posts, createPost]);
   }
 
-  const style = {backgroundColor : "#1f1f1f",
-    color : "beige",
-    borderRadius : 8,
-    padding : 8,
-    margin : 5,
-    border : "10px solid #1f1f1f"
-  }
   return (
     <div>
-      <button onClick={increment} style={style}>Chats {notificationCount}</button>
-    </div>
-  )
-
-}
-
-
-const style = {
-  width: 200,
-  "background-color": "white",
-  borderRadius: 10,
-  borderColor: "gray",
-  display: "flex",
-};
-
-function PostComponent({username, followerCount, time}) {
-  return (
-    <div>
-      <div style={style}>
-        <img
-          src={"./public/vite.svg"}
-          style={{ width: 35, height: 35, borderRadius: 25, margin: 10 }}
-        />
-        <div style={{ margin: 10 }}>
-          <b>{username}</b>
-          <div>
-            <span>{followerCount}</span>
-          </div>
-        </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "20px",
+          padding: "20px",
+          backgroundColor: "#f0f0f0",
+          borderRadius: 10,
+        }}
+      >
+        <button
+          onClick={addPost}
+          style={{
+            padding: "10px 20px",
+            backgroundColor: "#007BFF",
+            color: "white",
+            border: "none",
+            borderRadius: 5,
+            cursor: "pointer",
+            fontWeight: "bold",
+          }}
+        >
+          Add Post
+        </button>
+        <Notification />
       </div>
-       {time != undefined && <div style={{display : "flex", flexDirection : "row", margin : 10}}>
-          <div>{time}</div>
-          <div>
-            <img src={"./public/clock.avif"} style={{width : 20, height : 20, borderRadius : 20}}></img>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {posts.map((post) => (
+          <div
+            key={post.id}
+            style={{
+              backgroundColor: "#4682B4",
+              margin: 20,
+              width: 400,
+              padding: 10,
+              borderRadius: 15,
+            }}
+          >
+            <PostComponent
+              username={post.username}
+              followerCount={post.followerCount}
+              time={post.time}
+            />
           </div>
-        </div>}
-      <div style={{margin : 5}}>
-        <span style={{ color: "blue", fontWeight: "bold"}}>
-          Check out how these folks won $6000 in bounties!
-        </span>
+        ))}
       </div>
     </div>
-  )
+  );
 }
-
 
 export default App;
